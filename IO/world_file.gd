9e9ac6a44fd783901b48047d231db2a40174fe33
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-23 15:51:15 -0700

    Added basic saving functionality

--- IO/world_file.gd
+++ IO/world_file.gd
@@ -1,8 +1,8 @@
 class_name WorldFile extends Resource
 
-var name : StringName
-#var devNotes : String
+const VERSION := &"0.1 pre"
 
+var name : StringName
 var levels : Array[LevelData]
 
 class LevelData:
@@ -10,7 +10,6 @@ class LevelData:
 	
 	var position : Vector2i = Vector2i.ZERO
 	var size : Vector2i = Vector2i.ONE * 2
-	
 	var connections : PackedInt64Array
 
 static func loadFromFile(path: StringName) -> WorldFile:
@@ -21,4 +20,35 @@ static func loadFromFile(path: StringName) -> WorldFile:
 	return w
 
 func saveToFile(path: StringName) -> void:
-	pass
+	const VERSION_TAG := &"FlashViper WorldFile\nVersion %s"
+	const PROPERTY_TAG := &"%s: %s"
+	const LEVEL_TAG := &"Level %02d"
+	
+	var f := FileAccess.open(path, FileAccess.WRITE)
+	f.store_line(VERSION_TAG % VERSION)
+	
+	var properties := [
+		["name", name],
+#		["devNotes", name],
+	]
+	
+	for p in properties:
+		f.store_line(PROPERTY_TAG % p)
+	
+	properties = []
+	for i in levels.size():
+		var l := levels[i]
+		f.store_line(LEVEL_TAG % [i])
+		
+		properties.append_array([
+			["filePath", l.filePath],
+			["position", l.position],
+			["size", l.size],
+			["connections", l.connections],
+		])
+		
+		for p in properties:
+			f.store_line("\t" + (PROPERTY_TAG % p))
+		
+		if i < levels.size() - 1:
+			f.store_line(&"")
