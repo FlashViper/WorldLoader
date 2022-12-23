6e396b3a9f980ad169c273175cbe362d22aa0983
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-23 12:05:11 -0700

    Added minimum screen size property to project manager

--- IO/project_manager.gd
+++ IO/project_manager.gd
@@ -4,3 +4,8 @@ var projectName : StringName
 var notes : StringName
 
 var tileSize := Vector2i(64, 64)
+
+@onready var minimum_screen_size = Vector2i(
+	ProjectSettings.get_setting("display/window/size/viewport_width"),
+	ProjectSettings.get_setting("display/window/size/viewport_height")
+) / tileSize
