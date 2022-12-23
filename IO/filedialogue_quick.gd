1d707b0282387c8238d745a4a23a989e9605ea37
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-23 16:48:43 -0700

    added quick save window that just saves to a certain directory or string pattern

---
+++ IO/filedialogue_quick.gd
@@ -0,0 +1,25 @@
+extends Popup
+
+signal file_submitted(filename: String)
+
+@export var base_dir := &"res://Worlds/%s.wrld"
+var just_submitted : bool
+
+func _ready() -> void:
+	%Input_File.text_submitted.connect(self.submit)
+	%Input_Submit.pressed.connect(self.submit)
+	popup_hide.connect(self.rejected)
+
+func request_file() -> void:
+	popup_centered()
+	%Input_File.grab_focus()
+
+func submit(text: String) -> void:
+	file_submitted.emit(base_dir % text)
+	hide()
+
+func rejected() -> void:
+	if !just_submitted:
+		file_submitted.emit("")
+	
+	just_submitted = null
