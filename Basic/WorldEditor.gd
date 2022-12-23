1e75b1fc056fbf178181f85a088d5c4788a90c8f
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-23 15:20:19 -0700

    Added level placeholder

--- Basic/WorldEditor.gd
+++ Basic/WorldEditor.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 @export var tile_size := Vector2i(7,7)
+@export var levelPlaceholder : PackedScene
 
 @onready var preview := $LevelSizePreview
 @onready var camera : Camera2D = $Camera2D
@@ -36,20 +37,52 @@ func _input(event: InputEvent) -> void:
 
 func create_level(pos1: Vector2i, pos2: Vector2i) -> void:
 	var originalRect := Rect2i(pos1, Vector2()).expand(pos2)
+	var newRect := originalRect
 	
 	if originalRect.size < ProjectManager.minimum_screen_size:
 		var diff : Vector2i = max(ProjectManager.minimum_screen_size - originalRect.size, Vector2i())
-		originalRect = originalRect.expand(originalRect.get_center() + Vector2i(0.5 * (originalRect.size + diff)) * (pos1 - pos2).sign())
+		newRect = originalRect.expand(originalRect.get_center() + Vector2i(0.5 * (originalRect.size + diff)) * (pos1 - pos2).sign())
 		pass
 	
-	var r := preview.duplicate(0)
+	var r := levelPlaceholder.instantiate()
 	add_child(r)
-	r.position = grid_to_world(originalRect.position)
-	r.size = grid_to_world(originalRect.size)
-	r.border_color = Color.WHITE
+	
+	r.tile_size = tile_size
+	r.initialize(newRect, originalRect)
+
+func collideRect(target: Rect2i, collision: Rect2i, original : Rect2i) -> Rect2i:
+	if !target.intersects(collision):
+		return target
+	
+	var final := target
+	
+	var intersection := target.intersection(collision)
+	var isSizeChanged := target.size != original.size
+	var isPositionChanged := target.position != original.position
+	
+	if isSizeChanged:
+		var dir := intersection.size - target.size
+		final = final.grow_individual(
+			-abs(dir.x if dir.x < 0 else 0),
+			-abs(dir.x if dir.x > 0 else 0),
+			-abs(dir.y if dir.y < 0 else 0),
+			-abs(dir.y if dir.y > 0 else 0)
+		)
+	
+	if isPositionChanged:
+		var dir := (intersection.position - target.position).sign()
+		final.position -= dir * intersection.size
+	
+	return final
 
 func world_to_grid(world: Vector2) -> Vector2i:
 	return Vector2i(world) / tile_size
 
+func world_to_grid_rect(world: Rect2) -> Rect2i:
+	return Rect2i(world_to_grid(world.position), world_to_grid(world.size))
+
 func grid_to_world(grid: Vector2i) -> Vector2:
 	return Vector2(grid * tile_size)
+
+func grid_to_world_rect(grid: Rect2i) -> Rect2:
+	return Rect2(grid_to_world(grid.position), grid_to_world(grid.size))
---
+++ Basic/level_placeholder.gd
@@ -0,0 +1,47 @@
+# TODO: Create grid math helper class with macros like <convert_grid_to_tile>
+extends ReferenceRect
+
+const WorldEditor := preload("./WorldEditor.gd")
+
+@export var tile_size := Vector2i(7, 7)
+
+var rect : Rect2i :
+	set(new):
+		rect = new
+		if new.size < Vector2i.ONE:
+			pass
+var editedRect : Rect2
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				pass
+			else:
+				editedRect = Rect2(rect.position * tile_size, rect.size * tile_size)
+	
+	if event is InputEventMouseMotion:
+		if (event.button_mask & MOUSE_BUTTON_MASK_LEFT) > 0:
+			editedRect.position += event.relative
+			rect = Rect2i(
+				Vector2i(editedRect.position) / tile_size,
+				Vector2i(editedRect.size) / tile_size,
+			)
+			
+			updateTransform()
+
+func initialize(newSize: Rect2i, uncorrectedSize: Rect2i) -> void:
+	rect = newSize
+	editedRect = Rect2(rect.position * tile_size, rect.size * tile_size)
+	
+	position = uncorrectedSize.position * tile_size
+	size = uncorrectedSize.size * tile_size
+	updateTransform()
+
+func updateTransform() -> void:
+	var t := create_tween().set_parallel(true)
+	t.tween_property(self, "position", Vector2(rect.position * tile_size), 0.1).set_trans(Tween.TRANS_CUBIC)
+	t.tween_property(self, "size", Vector2(rect.size * tile_size), 0.1).set_trans(Tween.TRANS_CUBIC)
+
+func editRect() -> void:
+	pass
