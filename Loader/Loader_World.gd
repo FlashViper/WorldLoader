a14da54f22bd6b0a2383e85fa6f931e320dbb9d7
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-23 19:32:55 -0700

    Added quicksave support to world loader

--- Loader/Loader_World.gd
+++ Loader/Loader_World.gd
@@ -4,3 +4,15 @@ const LevelLoader := preload("./Loader_Level.gd")
 
 @onready var levelLoader : LevelLoader = %LevelLoader
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.keycode == KEY_O and event.is_pressed() and event.ctrl_pressed:
+			$open_dialogue.request_file()
+			var filepath : String = await $open_dialogue.file_submitted
+			if FileAccess.file_exists(filepath):
+				var f := load(filepath)
+				if f is WorldFile:
+					loadWorld(f)
+
+func loadWorld(w: WorldFile) -> void:
+	pass
