d98a3e1df021d3f17139f31683f2395fcab2d08c
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-23 19:30:46 -0700

    removed verbose debugging print statements

--- addons/importer_worldfile/plugin.gd
+++ addons/importer_worldfile/plugin.gd
@@ -7,12 +7,8 @@ var importer : WorldImporter
 func _enable_plugin() -> void:
 	importer = WorldImporter.new()
 	add_import_plugin(importer)
-	print("Initialized import plugin")
 
 func _disable_plugin() -> void:
-	print("Disabling plugin")
 	if importer:
 		remove_import_plugin(importer)
 		importer = null
-		print("Nullified importer")
-	print("Successfully disabled plugin")
