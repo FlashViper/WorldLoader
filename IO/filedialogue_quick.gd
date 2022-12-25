9906b0ab7867714a1260236c978ec73fcec812dd
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-25 14:06:58 -0700

    fixed quick dialogue hack from earlier

--- IO/filedialogue_quick.gd
+++ IO/filedialogue_quick.gd
@@ -1,21 +1,49 @@
+@tool
 extends Popup
 
 signal file_submitted(filename: String)
 
 @export var base_dir := &"res://Worlds/%s.wrld"
+
+@export_group("Create Folders")
+@export var allow_option_create_folders := true :
+	set(new):
+		allow_option_create_folders = new
+		
+		if input_create_folders:
+			input_create_folders.visible = new
+			
+@export var default_value_create_folders := true :
+	set(new):
+		default_value_create_folders = new
+		if input_create_folders:
+			input_create_folders.button_pressed = new
+
+@onready var input_text : LineEdit = %Input_File
+@onready var input_create_folders : Button = %Input_CreateFolders
+
 var just_submitted : bool
 
 func _ready() -> void:
-	%Input_File.text_submitted.connect(self.submit)
-	%Input_Submit.pressed.connect(func(): self.submit(%Input_File.text))
+	if Engine.is_editor_hint():
+		return
+	
+	%Input_Submit.pressed.connect(func(): self.submit(input_text.text))
+	input_text.text_submitted.connect(self.submit)
 	popup_hide.connect(self.rejected)
 
 func request_file() -> void:
+	if Engine.is_editor_hint():
+		return
+		
 	popup_centered()
-	%Input_File.grab_focus()
+	input_text.grab_focus()
 
 func submit(text: String) -> void:
-	if %Input_CreateFolders.button_pressed:
+	if Engine.is_editor_hint():
+		return
+	
+	if input_create_folders.button_pressed:
 		DirAccess.make_dir_recursive_absolute(base_dir.get_base_dir())
 	
 	file_submitted.emit(base_dir % text)
@@ -23,6 +51,9 @@ func submit(text: String) -> void:
 	hide()
 
 func rejected() -> void:
+	if Engine.is_editor_hint():
+		return
+	
 	if !just_submitted:
 		file_submitted.emit("")
 	
