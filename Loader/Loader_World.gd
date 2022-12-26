f478d5eeee8c84d747f0de2848afeb8c085ee94e
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-25 19:28:22 -0700

    Fixed bug where exported builds would stop before loading a file

--- Loader/Loader_World.gd
+++ Loader/Loader_World.gd
@@ -9,10 +9,10 @@ func _input(event: InputEvent) -> void:
 		if event.keycode == KEY_O and event.is_pressed() and event.ctrl_pressed:
 			$open_dialogue.request_file()
 			var filepath : String = await $open_dialogue.file_submitted
-			if FileAccess.file_exists(filepath):
-				var f := load(filepath)
-				if f is WorldFile:
-					loadWorld(f)
+			var f := ResourceLoader.load(filepath, "WorldFile")
+			
+			if f != null and f is WorldFile:
+				loadWorld(f)
 
 func loadWorld(w: WorldFile) -> void:
 	for l in w.levels:
