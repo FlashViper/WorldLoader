cd5f69603ac7e40d8a8e458c11a095c5d54a66d2
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-26 18:05:54 -0700

    Added getRect() function to LevelData

--- IO/LevelData.gd
+++ IO/LevelData.gd
@@ -6,3 +6,6 @@ class_name LevelData extends Resource
 @export var position : Vector2i = Vector2i.ZERO
 @export var size : Vector2i = Vector2i.ONE * 2
 @export var connections : PackedInt64Array
+
+func getRect() -> Rect2i:
+	return Rect2i(position, size)
