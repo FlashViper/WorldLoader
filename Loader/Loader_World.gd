b619e2bcc7c49553c90cfc03b14030baed28a342
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-26 18:22:36 -0700

    Added load screen functionality

--- Loader/Loader_World.gd
+++ Loader/Loader_World.gd
@@ -16,9 +16,44 @@ func _input(event: InputEvent) -> void:
 				loadWorld(f)
 
 func loadWorld(w: WorldFile, level := 0) -> void:
-	for l in w.levels:
-		createLevelRect(l)
+	if w.levels.size() < 1:
+		printerr("Tried to load a World File with 0 levels")
+		return
 	
+	world = w
+	
+	if level < 0 or level >= w.levels.size():
+		level = 0
+	
+	loadScreen(level)
+
+func loadScreen(level: int) -> void:
+	if !world:
+		printerr("Tried to load a screen on a null world")
+		return
+	
+	for l in levelObjs:
+		l.queue_free()
+	levelObjs = []
+	
+	currentLevel = level
+	createLevelRect(world.levels[currentLevel]) # replace with LevelLoader.load_level() in the future
+	
+	loaded = {currentLevel: rectToWorld(world.levels[currentLevel].getRect())}
+	
+	for c in world.levels[level].connections:
+		if c < 0 or c >= world.levels.size() or c == currentLevel:
+			continue
+		
+		loaded[c] = rectToWorld(world.levels[c].getRect())
+		createLevelRect(world.levels[c])
+	
+	# load the player in at the default position
+	player.position = world.levels[level].getRect().get_center() * ProjectManager.tileSize
+
+func rectToWorld(a: Rect2i) -> Rect2:
+	return Rect2(a.position * ProjectManager.tileSize, a.size * ProjectManager.tileSize)
+
 func createLevelRect(l: LevelData) -> void:
 	var r := ReferenceRect.new()
 	
@@ -29,4 +64,5 @@ func createLevelRect(l: LevelData) -> void:
 	r.position = l.position * ProjectManager.tileSize
 	r.size = l.size * ProjectManager.tileSize
 	
+	levelObjs.append(r)
 	add_child(r)
