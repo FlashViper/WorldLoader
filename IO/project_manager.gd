9fab20c00bbb6e0ef7481e07c122e28f528af159
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-12-26 17:59:19 -0700

    Changed tile size for testing

--- IO/project_manager.gd
+++ IO/project_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var projectName : StringName
 var notes : StringName
 
-var tileSize := Vector2i(64, 64)
+var tileSize := Vector2i(16, 16)
 
 @onready var minimum_screen_size = Vector2i(
 	ProjectSettings.get_setting("display/window/size/viewport_width"),
