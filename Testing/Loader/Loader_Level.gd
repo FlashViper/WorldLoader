5abd2bd70d2ec80597aefe6f34399da48840ae00
FlashViper <61369148+FlashViper@users.noreply.github.com>
2023-04-15 15:25:03 -0600

    respawn point testing in level loader

--- Testing/Loader/Loader_Level.gd
+++ Testing/Loader/Loader_Level.gd
@@ -1,17 +1,33 @@
 extends Node
 
-@onready var collisionMap := $CollisionMap
-var currentLevels : Dictionary
+@onready var collision_map := $CollisionMap
+var current_levels : Dictionary
 
 
 func _ready() -> void:
-	collisionMap.tileSize = Vector2i.ONE * ProjectManager.tile_size
-	var l := preload("res://Testing/Levels/jefaloosh.lvl")
+	collision_map.tileSize = Vector2i.ONE * ProjectManager.tile_size
+	var l := preload("res://Testing/Levels/respawn.lvl")
+	
+	print(inst_to_dict(l))
 	loadLevel(l)
 
 func loadLevel(level : LevelFile, position := Vector2i.ZERO) -> void:
-	collisionMap.load_collision_at_position(level, position)
+	collision_map.load_collision_at_position(level, position)
+	print(level.respawn_points)
+	for p in level.respawn_points:
+		print(level.respawn_points[p])
+		var s := Sprite2D.new()
+		s.texture = preload("res://GUI/Icons/LevelEditor/icon_level_leveldata.tres")
+		s.position = level.respawn_points[p]
+		add_child(s)
+	
+	var target_point := "entrance_nw"
+	var default_id : String = level.respawn_points.keys()[0]
+	var default_point : Vector2 = level.respawn_points[default_id]
+	$TestCharacter.position = level.respawn_points.get(target_point, default_point)
+	$TestCharacter.snap_to_ground()
 
-@warning_ignore(unused_parameter)
+
+@warning_ignore("unused_parameter")
 func unloadLevel(level: LevelFile) -> void:
-	collisionMap.clear_rect()
+	collision_map.clear_rect()
