c876245a4886db7052aa24eb362e2906f9c60cba
FlashViper <61369148+FlashViper@users.noreply.github.com>
2023-04-15 15:18:25 -0600

    added checks to level file display

--- addons/flashviper_worldfile/LevelFile/Inspector/display_levelfile.gd
+++ addons/flashviper_worldfile/LevelFile/Inspector/display_levelfile.gd
@@ -7,19 +7,23 @@ func display(level: LevelFile) -> void:
 		%s[color=darkgrey] by [/color]%s[color=darkgrey] tiles
 	" % [level.size.x, level.size.y]
 	
-	var img := Image.create(
-		level.size.x, 
-		level.size.y, 
-		false, 
-		Image.FORMAT_RGB8
-	)
-	
-	var index := 0
-	for y in level.size.y:
-		for x in level.size.x:
-			if level.tileData[index] != 0:
-				img.set_pixel(x,y, Color.WHITE)
-			index += 1
-	
-	var tex := ImageTexture.create_from_image(img)
-	%Bitmask.texture = tex
+	if level.size.x > 1 and level.size.y > 1:
+		var img := Image.create(
+			level.size.x, 
+			level.size.y, 
+			false, 
+			Image.FORMAT_RGB8
+		)
+		
+		if level.tileData.size() > 0:
+			var index := 0
+			for y in level.size.y:
+				for x in level.size.x:
+					if level.tileData[index] != 0:
+						img.set_pixel(x,y, Color.WHITE)
+					index += 1
+		
+		var tex := ImageTexture.create_from_image(img)
+		%Bitmask.texture = tex
+	else:
+		pass
