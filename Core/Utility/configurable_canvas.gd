053878840a0e3d7a178cb375f11cd69ec0286db8
FlashViper <61369148+FlashViper@users.noreply.github.com>
2023-04-15 15:21:01 -0600

    I don't remember doing this but it works so ok

--- Core/Utility/configurable_canvas.gd
+++ Core/Utility/configurable_canvas.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 var custom_controls : Array[Control] = []
 
-func add_control(control: Control, anchor : Control.LayoutPreset = -1) -> void:
+func add_control(control: Control, anchor := -1) -> void:
 	custom_controls.append(control)
 	add_child(control)
 	
